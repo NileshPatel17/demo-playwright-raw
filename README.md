3ca152e3338446dc14efdd67e9cfa844e217d79b
Nilesh Patel <nilesh.nvs@hotmail.com>
2022-05-20 12:59:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# setup
+Setup by
+
+```
+npm i
+```
+
+Now you can start live compiling by executing:
+
+```sh
+npm run dev
+```
+
+And then execute the compiled code with:
+
+```sh
+npm start
+```
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import playwright from 'playwright';
+
+async function runCronJob() {
+  console.log('Running job @ ', new Date().toLocaleTimeString())
+  /** Launch browser */
+  const browser = await playwright.chromium.launch({ headless: false });
+
+  /** Create a context */
+  const context = await browser.newContext();
+
+  /** Create a page */
+  const page = await context.newPage();
+
+  /** Carry out actions */
+  await page.goto('https://google.com');
+  await page.locator('[title="Search"]').fill('Microsoft Playwright');
+  await page.locator('[title="Search"]').press('Enter');
+  await page.locator('#result-stats').waitFor({ state: 'visible' });
+  await page.screenshot({ path: './screenshots/page.png' });
+
+  /** Close the browser */
+  await browser.close();
+}
+
+function main(){
+  runCronJob();
+  setInterval(runCronJob, 1 * 60 * 1000);
+}
+
+main();
